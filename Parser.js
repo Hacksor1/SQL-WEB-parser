f9793aef08fc141289d92a624b92aa27613289d3
Hacksor1 <66943886+Hacksor1@users.noreply.github.com>
2022-10-04 20:00:23 +0800

    Add files via upload

--- Parser.js
+++ Parser.js
@@ -5,7 +5,7 @@ const { JSDOM } = jsdom
 
 class Parser {
   // select <attrs> from <url> where [selector] like <regExp>
-  static async exec(query) {
+  static async exec(query) { // Выпонить запрос
     try {
       var { attrs, url, selector, filter } =
         /^SELECT \[(?<attrs>.*?)\] FROM (?<url>.*?) WHERE \[(?<selector>.*)\] ?(LIKE (?<filter>.*))?/i.exec(query).groups
@@ -13,7 +13,7 @@ class Parser {
       throw new Error('Wrong syntax')
     }
     console.log(attrs, url, selector, filter)
-    attrs = attrs.split(',').map(attrName => attrName.trim())
+    attrs = attrs.split(',').map(attrName => attrName.trim()) //превращаем строку в массив с удалением пробелов
     const res = await Parser._parseAttributes(url, attrs, selector, filter)
     return res
   }
@@ -26,11 +26,11 @@ class Parser {
     }
 
     const html = response.data
-    const document = new JSDOM(html).window.document
+    const document = new JSDOM(html).window.document //Превращаем в объект документ
     const elems = document.querySelectorAll(selector)
 
     const parseResult = {}
-    for (const attributeName of attributes) {
+    for (const attributeName of attributes) { //создаем множество для атрибутов
       parseResult[attributeName] = new Set()
     }
     const filteredElem = new Set()
@@ -39,11 +39,11 @@ class Parser {
         let attribute
         if (attributeName === 'innerHTML') {
           attribute = elem.innerHTML
-        } else if (elem.hasAttribute(attributeName)) {
+        } else if (elem.hasAttribute(attributeName)) { 
           attribute = elem.getAttribute(attributeName)
         }
 
-        const filterRegExp = new RegExp(filter, 'gmi')
+        const filterRegExp = new RegExp(filter, 'gmi') // фильтрация по значению атрибутов
         if (filterRegExp.test(attribute)) {
           parseResult[attributeName].add(attribute)
           filteredElem.add(elem)
@@ -51,13 +51,13 @@ class Parser {
       }
     }
 
-    if (attributes.includes('XPATH')) {
+    if (attributes.includes('XPATH')) { //зарпос атрибута
       for (const elem of Array.from(filteredElem)) {
         parseResult['XPATH'].add(createXPathFromElement(elem))
       }
     }
 
-    for (let [key, value] of Object.entries(parseResult)) {
+    for (let [key, value] of Object.entries(parseResult)) { // Все множества преврщаем в массив для JSON
       parseResult[key] = Array.from(value)
     }
     return parseResult
